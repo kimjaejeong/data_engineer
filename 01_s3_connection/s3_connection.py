a751d9e77dff0c9e27c581675984069937474688
김재정 <jaejeong.kim@kbfg.com>
2021-07-23 14:01:58 +0900

    디렉토리 구조 변경2

---
+++ 01_s3_connection/s3_connection.py
@@ -0,0 +1,20 @@
+import boto3
+import pandas as pd
+
+# Windows - CMD: aws configure
+AWS_ACCESS_KEY_ID = ''
+AWS_ACCESS_KEY_SECRET = ''
+region_name = ''
+
+client = boto3.client('s3') #low-level functional API
+resource = boto3.resource('s3') #high-level object-oriented API
+# my_bucket = resource.Bucket('my-bucket') #subsitute this for your s3 bucket name.
+
+for bucket in resource.buckets.all():
+    bucket_name = bucket.name
+
+# obj = client.get_object(Bucket=bucket_name, Key='test.csv')
+obj = client.get_object(Bucket=bucket_name, Key='test.csv')
+grid_sizes = pd.read_csv(obj['Body'])
+print(grid_sizes)
+
